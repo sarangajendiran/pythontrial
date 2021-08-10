6ed8f57057d00fa72930e95c0723c63e5c44907c
saran.gajendiran <saran.gajendiran@accesshq.com>
2021-08-11 09:50:02 +1000

    added rail

--- simple.py
+++ simple.py
@@ -1,2 +1,14 @@
 print("First git push")
 print("Second commit")
+import dotenv,os,testrail,Conf_Reader
+ 
+def get_testrail_client():
+    "Get the TestRail account credentials from the testrail.env file"
+    testrail_file = os.path.join(os.path.dirname(__file__),'testrail.env')
+    #Get the TestRail Url
+    testrail_url = Conf_Reader.get_value(testrail_file,'TESTRAIL_URL')
+    client = testrail.APIClient(testrail_url)
+    #Get and set the TestRail User and Password
+    client.user = Conf_Reader.get_value(testrail_file,'TESTRAIL_USER')
+    client.password = Conf_Reader.get_value(testrail_file,'TESTRAIL_PASSWORD')
+    return client
