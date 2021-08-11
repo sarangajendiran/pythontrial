af1100225a408b5ec03898ef32398959bccbcf9d
saran.gajendiran <saran.gajendiran@accesshq.com>
2021-08-11 17:31:27 +1000

    Reading TCs

--- simple.py
+++ simple.py
@@ -1,16 +1,59 @@
 import dotenv,os,testrail
 import Conf_Reader
+import pandas as pd
+import json
 def get_testrail_client():
-    
-    "Get the TestRail account credentials from the testrail.env file"
-    testrail_file = os.path.join(os.path.dirname(__file__),'testrail.env')
+    "Get the TestRail account credentials from the Conf_Reader.py"
     #Get the TestRail Url
     testrail_url = Conf_Reader.TESTRAIL_URL
     client = testrail.APIClient(testrail_url)
     #Get and set the TestRail User and Password
     client.user = Conf_Reader.TESTRAIL_USER
     client.password = Conf_Reader.TESTRAIL_PASSWORD
-    print("Connected to TestRail")
+    # print("Connected to TestRail")
     return client
-get_testrail_client()
 
+def get_project_id(project_name):
+    "Get the project ID using project name"
+    client = get_testrail_client()
+    project_id=None
+    projects = client.send_get('get_projects')
+    for project in projects:
+        if project['name'] == project_name: 
+            project_id = project['id']
+            #project_found_flag=True
+            break
+    return project_id
+
+def get_suites(project_name, suite_name):
+    "Get the project ID using project name"
+    client = get_testrail_client()
+    project_id=get_project_id(project_name)
+    print("Project ID:", project_id)
+    suite_id = None
+    Suites = client.send_get('get_suites/%s' %project_id)
+    for suite in Suites:
+        if suite['name'] == suite_name: 
+            suite_id = suite['id']
+            print("Suite ID:", suite_id)
+            break
+    return suite_id, project_id
+
+def get_cases(project_name, suite_name):
+    "Get the project ID using project name"
+    client = get_testrail_client()
+    suite_id, project_id = get_suites(project_name, suite_name)
+    print("Suites for the project ID:", suite_id)
+    cases = client.send_get('get_cases/%s&suite_id=%s' %(project_id,suite_id))
+    file1 = open(r'C:\Users\4448\Read.json', 'w')
+    case = None
+    for case in cases:
+        file1.write(str(case))
+        file1.write('\n')
+    file1.close()
+    return cases
+
+# cases = get_cases('Project DELTA E2E', '14.Security & Access Management')
+df = pd.read_csv(r'C:\Users\4448\Read.txt', header = None)
+print(df.shape)
+df.to_excel(r'C:\Users\4448\Read_data.xlsx')
