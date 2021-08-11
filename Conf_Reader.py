4326c6663c7576b2e074a21eb6f66dcc43a0bee5
saran.gajendiran <saran.gajendiran@accesshq.com>
2021-08-11 10:07:16 +1000

    TestRail

---
+++ Conf_Reader.py
@@ -0,0 +1,3 @@
+TESTRAIL_URL='https://defencehealthltd.testrail.io/index.php?/dashboard'
+TESTRAIL_USER='saranraj.gajendiran@defencehealth.com.au'
+TESTRAIL_PASSWORD=''
--- simple.py
+++ simple.py
@@ -1,14 +1,16 @@
-print("First git push")
-print("Second commit")
-import dotenv,os,testrail,Conf_Reader
- 
+import dotenv,os,testrail
+import Conf_Reader
 def get_testrail_client():
+    
     "Get the TestRail account credentials from the testrail.env file"
     testrail_file = os.path.join(os.path.dirname(__file__),'testrail.env')
     #Get the TestRail Url
-    testrail_url = Conf_Reader.get_value(testrail_file,'TESTRAIL_URL')
+    testrail_url = Conf_Reader.TESTRAIL_URL
     client = testrail.APIClient(testrail_url)
     #Get and set the TestRail User and Password
-    client.user = Conf_Reader.get_value(testrail_file,'TESTRAIL_USER')
-    client.password = Conf_Reader.get_value(testrail_file,'TESTRAIL_PASSWORD')
+    client.user = Conf_Reader.TESTRAIL_USER
+    client.password = Conf_Reader.TESTRAIL_PASSWORD
+    print("Connected to TestRail")
     return client
+get_testrail_client()
+
