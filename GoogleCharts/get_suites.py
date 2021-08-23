84b9620dabe11e690555d0a38014cfa9a8e981f9
saran.gajendiran <saran.gajendiran@accesshq.com>
2021-08-23 10:39:31 +1000

    adding static folder and image

--- GoogleCharts/get_suites.py
+++ GoogleCharts/get_suites.py
@@ -29,6 +29,10 @@ def get_cases(suite_id):
     "Get the project ID using project name"
     client = get_testrail_client()
     cases = client.send_get('get_cases/25&suite_id=%s' %suite_id)
+    if suite_id == 138:
+        file1 = open(r'C:\Users\4448\sample.txt', 'w')
+        file1.write(str(cases))
+        file1.close()
     total_cases = len(cases)
     return total_cases
 
@@ -41,6 +45,8 @@ def get_suites(project_name):
     suites = client.send_get('get_suites/%s' %project_id)
     df1 = pd.DataFrame(suites, columns = ['id','name'])
     df1['total_cases'] = df1.apply(lambda x: get_cases(x['id']), axis=1)
+    test_cases = df1['total_cases'].sum()
+    print("Total Test Cases created so far", test_cases)
     a = df1.columns.values.tolist()
     b = df1.values.tolist()
     tempdata = json.dumps({'title': a, 'data':b})
@@ -53,15 +59,17 @@ def get_runs():
     "Get the project ID using project name"
     client = get_testrail_client()
     reports = client.send_get('get_runs/25')
-    df1 = pd.DataFrame(reports, columns = ['name','passed_count', 'blocked_count', 'untested_count','retest_count','failed_count'])
-    df1.rename(columns = {'name' : 'Test Runs Name','passed_count' : 'Passed', 'blocked_count' : 'Blocked', 'untested_count' : 'UnTested','retest_count' : 'Retested','failed_count': 'Failed'}, inplace= True)
+    # df2 = pd.DataFrame(reports)
+    # file = df2.to_csv(r'C:\Users\4448\testrunsfull.csv', index= False)
+    df1 = pd.DataFrame(reports, columns = ['name','passed_count', 'blocked_count', 'untested_count','retest_count','failed_count','custom_status1_count','custom_status3_count'])
+    df1.rename(columns = {'name' : 'Test Runs Name','passed_count' : 'Passed', 'blocked_count' : 'Blocked', 'untested_count' : 'UnTested','retest_count' : 'Retested','failed_count': 'Failed','custom_status1_count': 'InProgress','custom_status3_count': 'Deferred'}, inplace= True)
     # print(df1)
     file = df1.to_csv(r'C:\Users\4448\testruns.csv', index= False)
-    print("File generated")
+    # print("File generated")
     a = df1.columns.values.tolist()
     b = df1.values.tolist()
     b.insert(0,a)
-    print(b)
+    # print(b)
     tempdata1 = json.dumps({'title': a, 'data':b})
     return tempdata1
 
